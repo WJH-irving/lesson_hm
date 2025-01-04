d6db4278857c86c533fc1d94f6ed6e9a372a61bf
Alicia-irving <2354602665@qq.com>
2025-01-04 10:33:12 +0800

    vue-router-demo

---
+++ vue_react/vue-router-demo/src/views/post/show/post-show.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div>PostShow {{ postId }}</div>
+    </div>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router'
+const route = useRoute() // 当前路由
+// console.log(route);
+const {postId}= route.params // 当前路由参数  
+</script>
+
+<style scoped>
+
+</style>

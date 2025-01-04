e1474e68b170e27199c86aa779164a06607c9edb
Alicia-irving <2354602665@qq.com>
2025-01-04 10:34:02 +0800

    BMW

---
+++ vue_react/todos/BMW/src/components/LifecycleComponent.vue
@@ -0,0 +1,67 @@
+<template>
+    <div>
+        <p id = "count">Lifecyle Component - Count:{{ count }}</p>
+    </div>
+  </template>
+  
+  <script setup>
+  import { defineProps,
+     onBeforeMount ,
+     onMounted,
+     onBeforeUnmount ,
+     onUnmounted ,
+     onBeforeUpdate, 
+     onUpdated 
+    } from 'vue';
+
+  defineProps({
+    count: {
+      type: Number,
+      required: true
+    }
+  })
+
+//   console.log('haha');
+ 
+  // 组件有生老病死 
+  onMounted(() => {
+    // on 某个阶段
+    // mounted 挂载
+    // 已经完成了挂载
+    console.log('已经挂载');
+    console.log(document.getElementById('count'),'--------------');
+  }) 
+  onBeforeMount(() => {
+    // before 某个阶段
+    // mount 挂载
+    // 即将挂载
+    console.log(document.getElementById('count'),'--------------');
+    console.log('即将挂载');
+  })
+  onBeforeUnmount(() => {
+    // before 某个阶段
+    // unmount 卸载
+    // 即将卸载
+    console.log('即将卸载');
+  })
+  onUnmounted(() => {
+    // unmount 卸载
+    // 已经卸载
+    console.log('已经卸载');
+  })
+  onBeforeUpdate(() => {
+    // before 某个阶段
+    // update 更新
+    // 即将更新
+    console.log('即将更新');
+  })
+  onUpdated(() => {
+    // update 更新
+    // 已经更新
+    console.log('已经更新');
+  })
+  </script>
+  
+  <style scoped>
+  
+  </style>
